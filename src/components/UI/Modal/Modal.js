ac1e3ce21f710906fcb5c15abaacd070460cea9b
El-Nico <nicholasc1665@yahoo.com>
2021-03-09 15:21:37 +0000

    added axios and firebase

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,7 +7,7 @@ import Backdrop from '../Backdrop/Backdrop';
 class Modal extends Component {
 
     shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show;
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
     componentWillUpdate () {
@@ -30,5 +30,6 @@ class Modal extends Component {
         )
     }
 }
+
 
 export default Modal;
